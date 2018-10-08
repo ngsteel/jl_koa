aa92ebab6d1fcd1fcf9849b744d2df5b95d9f00b
xujinglong <xujinglong@jd.com>
2018-10-08 17:34:26 +0800

    上传

--- src/index.js
+++ src/index.js
@@ -6,7 +6,8 @@ const render = require('koa-art-template');
 
 const app = new Koa();
 
-console.log('__dirname = ', __dirname)
+//console.log('__dirname = ', __dirname)
+
 
 render(app, {
     root: path.join(__dirname, 'views'),
@@ -14,23 +15,51 @@ render(app, {
     debug: process.env.NODE_ENV !== 'production'
 });
 
-// response
+/**
+ * 系统级中间件
+ *
+ */
 app.use((ctx, next) => {
 
-    ctx.body = 'Hello Koa';
+    ctx.body = '未匹配到任何资源。';
 
     next();
 });
 
 
-router.get('/user/:id', async (ctx) => {
-    let userInfo = {
-        title: '用户信息',
-        cname: '许井龙',
-        id: ctx.params.id
-    }
-    await ctx.render('user', userInfo);
-});
+/**
+ * 用户处理逻辑
+ */
+router
+    .get('/users/:id',async (ctx, next) => { /* 查看方法 */
+        let userInfo = {
+            title: '用户信息',
+            cname: '许井龙',
+            id: ctx.params.id /* 动态URL */
+        }
+
+        console.log(ctx.query.key); /* get 查询参数 */
+
+        await ctx.render('user', userInfo); /* 模板渲染 */
+
+        console.log('查看')
+
+    })
+    .post('/users', (ctx, next) => { /* 新增方法 */
+        // ...
+        console.log('新增')
+    })
+    .put('/users/:id', (ctx, next) => { /* 更新方法 */
+        // ...
+        console.log('更新')
+    })
+    .del('/users/:id', (ctx, next) => { /* 删除方法 */
+        console.log('删除')
+        // ...
+    })
+    .all('/users/:id', (ctx, next) => {
+        // ...
+    });
 
 
 app
